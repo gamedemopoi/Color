912e09af38089523ba521e780bd027c41de4e26a
ogata.fumitoshi <oggata@gmail.com>
2014-07-23 12:00:31 +0900

    change images

--- src/Manager/Stage.js
+++ src/Manager/Stage.js
@@ -17,12 +17,12 @@ var Stage = cc.Class.extend({
         this.isColored        = false;
         this.isEscaped        = false;
         this.clearTargetCnt   = 0;
-/*
+
         this.field = cc.Sprite.create(s_field);
         this.field.setScale(1.8,1.3);
         this.field.setPosition(820,330);
         this.game.mapNode.addChild(this.field);
-*/
+
         this.escape = cc.Sprite.create(s_scape_zone);
         this.escape.setPosition(800,560);
         this.game.mapNode.addChild(this.escape);
